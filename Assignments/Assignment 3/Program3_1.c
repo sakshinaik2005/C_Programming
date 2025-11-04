5f72541adc6287af92dd4e0e3c7677415042dac1
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-11-04 13:25:49 +0530

    Create Program3_1.c
    This C program prints the first iNo even numbers based on the user’s input. The function PrintEven(int iNo) first checks if the input is less than or equal to 0 and returns immediately in that case. Otherwise, it uses a for loop from 1 to iNo and prints each even number by multiplying the loop counter i by 2. In the main() function, the user is prompted to enter a number, which is then passed to PrintEven() to display the even numbers consecutively. This program demonstrates the use of loops and arithmetic operations to generate a sequence of numbers. The time complexity is O(n), where n is the input number iNo.

---
+++ Assignments/Assignment 3/Program3_1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void PrintEven(int iNo)
+{
+    int i = 0;
+    if(iNo <= 0)
+    {
+        return;
+    }
+    for(i = 1;i <= iNo;i++)
+    {
+        printf("%d",i*2);
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    PrintEven(iValue);
+
+    return 0;
+}
