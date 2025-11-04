761daff27248d9ea2d2571b0b8fb00993d0aeab5
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-11-04 13:42:36 +0530

    Create Program5_3.c
    This C program checks whether a given year is a leap year or not. It defines a function CheckLeapYear that takes an integer year as input. The function uses the standard rules for leap years: a year is a leap year if it is divisible by 4 and not divisible by 100, or if it is divisible by 400. If the year satisfies these conditions, the program prints that it is a leap year; otherwise, it prints that it is not a leap year. In the main function, the program prompts the user to enter a year, reads the input using scanf, and then calls the CheckLeapYear function to perform the check. The time complexity of this program is O(1) because the computation involves only a few arithmetic and logical operations, independent of the value of the input year.

---
+++ Assignments/Assignment 5/Program5_3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void CheckLeapYear(int year)
+{
+  if((year % 4 == 0 && year % 100 != 0)||(year % 400 == 0))
+  {
+    printf("%d is a leap year\n",year);
+  }
+  else
+  {
+    printf("%d is not a leap year\n",year);
+  }
+}
+int main()
+{
+    int yr;
+
+    printf("Enter year: ");
+    scanf("%d",&yr);
+
+    CheckLeapYear(yr);
+
+    return 0;
+}
