46785b5ddece1133bece5db6ca5ac84f22744aea
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-11-23 04:00:01 +0530

    Create Program5.c
    This program accepts N numbers from the user along with a number iValue to search for.
    The Frequency() function counts how many times the given number (iValue) appears in the array.
    Finally, the program displays the frequency of that number.

---
+++ Assignment22/Program5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Frequency(int Arr[], int iLength, int iNo)
+{
+    int iCnt = 0, iFrequency = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            iFrequency++;
+        }
+    }
+    return iFrequency;
+}
+
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0, iValue = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iSize);
+
+    printf("Enter the number to search: ");
+    scanf("%d", &iValue);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements:\n", iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d: ", iCnt + 1);
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Frequency(p, iSize, iValue);
+
+    printf("Frequency of %d is: %d\n", iValue, iRet);
+
+    free(p);
+
+    return 0;
+}
