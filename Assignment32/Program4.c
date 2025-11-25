cdc591eb69fcfd91cbfaaf2e2b3252a6a0b3c167
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-11-25 23:56:13 +0530

    Create Program4.c

---
+++ Assignment32/Program4.c
@@ -0,0 +1,54 @@
+/*
+   Input : iRow = 6    iCol = 6
+   Output :  *   *   *   *   *   *
+             *   #   #   #   *   *
+             *   #   #   *   $   *
+             *   #   *   $   $   *
+             *   *   $   $   $   *
+             *   *   *   *   *   *
+*/
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    for(int i = 1; i <= iRow; i++)
+    {
+        for(int j = 1; j <= iCol; j++)
+        {
+            if(i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("*\t");
+            }
+            else if(i < j && i + j < iRow + 1)
+            {
+                printf("#\t");
+            }
+            else if(i > j && i + j > iRow + 1)
+            {
+                printf("$\t");
+            }
+            else if(i == j || (i + j) == (iRow + 1))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("\t");   
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
