03111f14ffa4bcc4040af340f385023632ef5096
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-04 14:35:00 +0530

    Create Program1.c
    This program takes a string from the user and converts all uppercase alphabetic characters into lowercase. It iterates through each character of the string and checks if it lies between ‘A’ and ‘Z’. If so, it adds 32 to convert it to the corresponding lowercase letter based on ASCII values. Finally, it displays the modified lowercase string as output.

---
+++ Assignments/Assignment36/Program1.c
@@ -0,0 +1,29 @@
+//write a program which accept string from user and convert it into lower case
+
+#include <stdio.h>
+
+void strlwrx(char *str)
+{
+    while (*str != '\0')  
+    {
+        if (*str >= 'A' && *str <= 'Z')  
+        {
+            *str = *str + 32;  
+        }
+        str++;  
+    }
+}
+
+int main()
+{
+    char arr[20];
+
+    printf("Enter string: ");
+    scanf("%[^'\n']s", arr);
+
+    strlwrx(arr);
+
+    printf("Modified string is: %s\n", arr);
+
+    return 0;
+}
