02ad382c1d87e4688bf92f483b8f862ecc87eab4
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-04 14:37:37 +0530

    Create Program4.c
    This program accepts a string from the user and displays only the digit characters present in it. It iterates through the string one character at a time and checks whether each character lies between ‘0’ and ‘9’. Whenever a numeric character is found, it is printed to the output without altering the rest of the string. After scanning the entire input, the program displays all extracted digits together.

---
+++ Assignments/Assignment36/Program4.c
@@ -0,0 +1,29 @@
+//write a program which accept string from user and display only digits from that string
+
+#include <stdio.h>
+
+void DisplayDigit(char *str)
+{
+    printf("Digits in the string are: ");
+    while (*str != '\0')  
+    {
+        if (*str >= '0' && *str <= '9')  
+        {
+            printf("%c", *str);
+        }
+        str++;  
+    }
+    printf("\n");
+}
+
+int main()
+{
+    char arr[20];
+
+    printf("Enter string: ");
+    scanf("%[^'\n']s", arr);
+
+    DisplayDigit(arr);
+
+    return 0;
+}
