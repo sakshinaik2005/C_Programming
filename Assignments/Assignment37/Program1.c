62bd6eae720b528beaea6a20dd3c05059d2a9ac1
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-04 14:40:18 +0530

    Create Program1.c
    This program takes a string and a character from the user and checks whether the given character exists in the string. It scans the string one character at a time and compares each character with the user-entered character. If a match is found, the function immediately returns TRUE; otherwise, it continues until the end of the string. After processing, the program displays whether the character is present or not.

---
+++ Assignments/Assignment37/Program1.c
@@ -0,0 +1,47 @@
+//write a program which accept string from user and accept one character.check whether  that charater is present in string or not
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkChar(char *str, char ch)
+{
+    while (*str != '\0')  
+    {
+        if (*str == ch)  
+        {
+            return TRUE;
+        }
+        str++;
+    }
+    return FALSE;
+}
+
+int main()
+{
+    char arr[20];
+    char cValue;
+    BOOL bRet = FALSE;
+
+    printf("Enter string: ");
+    scanf("%[^'\n']s", arr);
+
+    printf("Enter the character: ");
+    scanf(" %c", &cValue);  
+
+    bRet = ChkChar(arr, cValue);
+
+    if (bRet == TRUE)
+    {
+        printf("Character found\n");
+    }
+    else
+    {
+        printf("Character not found\n");
+    }
+
+    return 0;
+}
