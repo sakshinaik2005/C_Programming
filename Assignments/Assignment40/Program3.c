918f67c8b4614bfd4fe9ef8b34db87a7e4693002
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-15 19:00:21 +0530

    Create Program3.c

---
+++ Assignments/Assignment40/Program3.c
@@ -0,0 +1,30 @@
+/*
+   write a recersive program which display below pattern
+
+   Input : 5
+   Output : 5  4  3  2  1  
+*/
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo == 0)
+    {
+        return;
+    }
+    printf("%d\t",iNo);
+    Display(iNo - 1);   
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the Number: ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
