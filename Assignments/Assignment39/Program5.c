f33433ad29a4d4495d848bfe375b116a9af1d53c
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-15 18:56:22 +0530

    Create Program5.c

---
+++ Assignments/Assignment39/Program5.c
@@ -0,0 +1,25 @@
+/*
+write a recursive program which display below pattern
+  Output : a  b  c  d  e  f
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+    static char ch = 'a';
+
+    if(ch <= 'f')
+    {
+        printf("%c\t",ch);
+        ch++;
+        Display();  
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
