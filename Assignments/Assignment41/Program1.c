dbdb845787ccb27263aa1ab3699019263fd86993
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-15 19:04:19 +0530

    Create Program1.c

---
+++ Assignments/Assignment41/Program1.c
@@ -0,0 +1,31 @@
+/*
+    write a recursive program which accept number from user and display below pattern
+
+    Input : 5
+    Output : 5 * 4 * 3 * 2 * 1 * 
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+     if(iNo == 0)
+    {
+        return;
+    }
+
+    printf("%d *\t", iNo);
+    Display(iNo - 1);
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
