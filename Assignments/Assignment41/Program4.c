808100561eccc29db4b62140969507dbf3af2bc5
sakshinaik2005 <sakshi36.naik@gmail.com>
2025-12-15 19:06:16 +0530

    Create Program4.c

---
+++ Assignments/Assignment41/Program4.c
@@ -0,0 +1,33 @@
+/*
+    write a recursive program which accept number from user and return its factorial
+
+    Input : 5
+    Output : 120 
+*/
+
+#include<stdio.h>
+
+int Fact(int iNo)
+{
+    if(iNo == 0)
+    {
+        return 1;
+    }
+
+    return iNo * Fact(iNo - 1);
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = Fact(iValue);
+
+    printf("%d", iRet);
+
+    return 0;
+}
