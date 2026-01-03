35a93f444060ae2802c6404cab4edae9d9a8c902
sakshinaik2005 <sakshi36.naik@gmail.com>
2026-01-03 22:44:27 +0530

    Create Program1.c

---
+++ Assignments/Assignment49/Program1.c
@@ -0,0 +1,283 @@
+/*  
+    1. Find Difference(Max - Min)
+    int Difference(PNODE Head);
+
+    | 11 | -> | 22 | -> | 51 | -> NULL
+    Number of nodes are : 3
+    | 11 | -> | 22 | -> | 51 | -> | 40 | -> | 17 | -> | 12 | -> NULL
+    Number of nodes are : 6
+    | 22 | -> | 51 | -> | 40 | -> | 17 | -> | 12 | -> NULL
+    Number of nodes are : 5
+    | 22 | -> | 51 | -> | 40 | -> | 17 | -> NULL
+    Number of nodes are : 4
+    | 22 | -> | 51 | -> | 105 | -> | 40 | -> | 17 | -> NULL
+    Number of nodes are : 5
+    | 22 | -> | 51 | -> | 40 | -> | 17 | -> NULL
+    Number of nodes are : 4
+    Difference (Max - Min) in the list: 34
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE temp = NULL;
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newn;
+    }
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+    else if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        temp = *first;
+        *first = (*first)->next;
+        free(temp);
+    }
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+        return;
+    else if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else
+    {
+        temp = *first;
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | -> ", first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+    return iCount;
+}
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+    int iSize = 0;
+    int iCnt = 0;
+
+    PNODE temp = NULL;
+    PNODE newn = NULL;
+
+    iSize = Count(*first);
+    
+    if((pos < 1) || (pos > iSize + 1))
+    {
+        printf("Invalid position\n");
+        return;
+    }
+    if(pos == 1)
+    {
+        InsertFirst(first, no);
+    }
+    else if(pos == iSize + 1)
+    {
+        InsertLast(first, no);
+    }
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+        newn->data = no;
+        newn->next = NULL;
+
+        temp = *first;
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+    int iSize = 0;
+    int iCnt = 0;
+
+    PNODE temp = NULL;
+    PNODE target = NULL;
+
+    iSize = Count(*first);
+
+    if((pos < 1) || (pos > iSize))
+    {
+        return;
+    }
+    if(pos == 1)
+    {
+        DeleteFirst(first);
+    }
+    else if(pos == iSize)
+    {
+        DeleteLast(first);
+    }
+    else
+    {
+        temp = *first;
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+        temp->next = target->next;
+        free(target);
+    }
+}
+
+int Difference(PNODE head)
+{
+    if(head == NULL)
+    {
+        return 0; 
+    }
+
+    int iMax = head->data;
+    int iMin = head->data;
+
+    while(head != NULL)
+    {
+        if(head->data > iMax)
+            iMax = head->data;
+        if(head->data < iMin)
+            iMin = head->data;
+        head = head->next;
+    }
+
+    return iMax - iMin;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 22);
+    InsertFirst(&head, 11);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+    
+    InsertLast(&head, 40);
+    InsertLast(&head, 17);
+    InsertLast(&head, 12);
+
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteFirst(&head);
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteLast(&head);
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    InsertAtPos(&head, 105, 3);
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteAtPos(&head, 3);
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n", iRet);
+
+    iRet = Difference(head);
+    printf("Difference (Max - Min) in the list: %d\n", iRet);
+
+    return 0;
+}
